526e0b4bf6c32e1b820f2b635ee3407555c61110
ecp-alex <alex.mackenzie@englishcollege.cz>
2020-01-24 14:38:30 +0100

    ROUTING | add routing to solution

--- Frontend/amactive-api-movie-db/src/index.js
+++ Frontend/amactive-api-movie-db/src/index.js
@@ -5,18 +5,41 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers/index";
 import "./index.css";
-// import App from './App';
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Error from "./pages/error";
 import BoxContainer from "./containers/box-container";
 
 let store = createStore(reducers, applyMiddleware(thunk));
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
   render() {
+    // console.log("[App.js] render... ");
+    console.log("[App.js] render... ");
+
     return (
-      <div>
-        <p>[index.js]</p>
-        <BoxContainer />
+      <div className="App">
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <div>
+                <p>[index.js]</p>
+                <BoxContainer />
+              </div>
+            </Route>
+
+            <Route component={Error} />
+          </Switch>
+        </main>
       </div>
     );
   }
@@ -24,7 +47,9 @@ class App extends React.Component {
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
---
+++ Frontend/amactive-api-movie-db/src/pages/error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Error() {
+  return (
+    <Link to="/" className="btn-primary">
+      Return home
+    </Link>
+  );
+}
